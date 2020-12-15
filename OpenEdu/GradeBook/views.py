4886f175b76ee0879cfe0a1e060ddceb4917d80e
Rostyk Lukavyi <rostyk@MacBook-Pro-Rostyk.local>
2020-12-15 20:19:45 +0200

    added diary to the page

--- OpenEdu/GradeBook/views.py
+++ OpenEdu/GradeBook/views.py
@@ -7,9 +7,20 @@ from core.models import GradeBook, BookColumn
 def gradebookteacher(request, id):
     get_columns = BookColumn.objects.all().filter(gradebook=id)
     book = GradeBook.objects.get(id=id)
+    data = []
+    i = 0
+    for colum in book.get_grades():
+        data.append([])
+        for grade in colum:
+            data[i].append({"date": str(grade.date.date), "grades": str(grade.value), "student": str(grade.user)})
+        i = i + 1
+
+    print(data)
+
     context = {
         'get_columns': get_columns,
         'get_check': book.get_grades(),
+        'raw_data': data,
     }
     template = 'GradeBook/diary.html'
     return render(request, template, context)
