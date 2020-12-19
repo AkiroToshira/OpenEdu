70fbcd877477c22dd44c47f0e3253e7a6aa89b83
OlehOrenchuk <Oleh.Orenchuk.KNM.2019@lpnu.ua>
2020-12-19 19:09:57 +0200

    Update utils.py

--- OpenEdu/OpenEdu/utils.py
+++ OpenEdu/OpenEdu/utils.py
@@ -1,13 +1,11 @@
-from django.contrib.auth.models import User, Group
-
+from django.contrib.auth.models import User
+from core.models import Group
 
 def count_users():
     return User.objects.count()
 
-
 def count_groups():
     return Group.objects.count()
-
 
 def has_perms_to_users(request):
     return request.user.is_authenticated and request.user.is_superuser
