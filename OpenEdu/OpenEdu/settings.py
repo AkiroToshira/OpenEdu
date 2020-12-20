21e06ca8ed67d57c2bddb93e6698039f443d74eb
OlehOrenchuk <Oleh.Orenchuk.KNM.2019@lpnu.ua>
2020-12-20 12:33:20 +0200

    Update settings.py

--- OpenEdu/OpenEdu/settings.py
+++ OpenEdu/OpenEdu/settings.py
@@ -3,7 +3,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -32,6 +31,7 @@ INSTALLED_APPS = [
     'Shedule',
     'GradeBook',
     'AdminPanel',
+    'admincolors',
 ]
 
 MIDDLEWARE = [
@@ -46,10 +46,16 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'OpenEdu.urls'
 
+ADMIN_COLORS = [
+    ('Default', 'admincolors/css/gray.css'),
+    ('Lite Blue', 'admincolors/css/lite.css'),
+    ('Dark Blue', 'admincolors/css/dark-blue.css'),
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -57,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'admincolors.context_processors.admin_theme',
             ],
         },
     },
@@ -72,16 +79,20 @@ WSGI_APPLICATION = 'OpenEdu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db',
+        'NAME': 'openedu',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': '4132',
+        'PASSWORD': 'olko2001',
     }
 }
-
-
-
+"""
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+}
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -117,7 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
