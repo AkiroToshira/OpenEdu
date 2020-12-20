d34693db45d4f2d9e1f54dde74fef7e90df199d5
OlehOrenchuk <Oleh.Orenchuk.KNM.2019@lpnu.ua>
2020-12-20 12:33:37 +0200

    Update utils.py

--- OpenEdu/OpenEdu/utils.py
+++ OpenEdu/OpenEdu/utils.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import User, Group
-
+from django.contrib.auth.models import User
+from core.models import Group
 
 def count_users():
     return User.objects.count()
